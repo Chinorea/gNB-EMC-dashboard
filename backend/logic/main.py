e629b829d9b3587ced88cdf30f879dd172d59aeb
Kenneth <63093454+Chinorea@users.noreply.github.com>
2025-05-19 10:38:35 +0800

    Added Board DateTime logic
    Added board date time display logic
    Added printing current datetime in main

--- backend/logic/main.py
+++ backend/logic/main.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3
+#Input command ":set fileformat=unix" on new file upload to board
 from attributes.IpAddress import IpAddress
+from attributes.BoardDateTime import BoardDateTime
 
 xml_path = "/cu/config/me_config.xml"
 ip = IpAddress(xml_path)
+boardDateTime = BoardDateTime()
 
 ip.refresh()
 
+boardDateTime.print_Current_time()
 ip.print_Ip_Status()
