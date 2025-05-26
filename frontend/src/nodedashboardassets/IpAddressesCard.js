c56a7abf5d1f432169499499ad086204e07de96b
Ethan <familychan1@gmail.com>
2025-05-26 14:11:39 +0800

    Split layer 3

---
+++ frontend/src/nodedashboardassets/IpAddressesCard.js
@@ -0,0 +1,84 @@
+// frontend/src/nodedashboardassets/IpAddressesCard.js
+import React from 'react';
+import { Card, CardContent, Typography, Grid } from '@mui/material';
+
+export default function IpAddressesCard({ data, isLoading }) {
+  if (!data) {
+    return null;
+  }
+
+  return (
+    <Grid item xs={12} md={4} sx={{ display: 'flex' }}>
+      <Card
+        elevation={3}
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          flex: 1,
+          transition: 'transform 0.1s ease-in-out',
+          '&:hover': {
+            transform: 'scale(1.01)',
+            boxShadow: 6,
+          },
+          backgroundColor: '#fff'
+        }}
+      >
+        <CardContent sx={{ textAlign: 'center', py: 1 }}>
+          <Typography
+            color="textSecondary"
+            gutterBottom
+            variant="subtitle2"
+            sx={{ fontSize: '1.2rem' }}
+          >
+            IP Addresses
+          </Typography>
+          <Grid container spacing={1} sx={{ mt: 3 }}>
+            <Grid item xs={4}>
+              <Typography
+                color="textSecondary"
+                gutterBottom variant="subtitle2"
+                sx={{fontSize: '1.0rem'}}
+              >
+                gNB IP
+              </Typography>
+              <Typography variant="body1" sx={{
+                fontWeight: 'bold',
+                fontSize: '1.5rem'
+                }}>
+                {data.ip_address_gnb}
+              </Typography>
+            </Grid>
+            <Grid item xs={4}>
+              <Typography
+                color="textSecondary"
+                gutterBottom variant="subtitle2"
+                sx={{fontSize: '1.0rem'}}>
+                NgC IP
+              </Typography>
+              <Typography variant="body1" sx={{
+                fontWeight: 'bold',
+                fontSize: '1.5rem'
+                }}>
+                {data.ip_address_ngc}
+              </Typography>
+            </Grid>
+            <Grid item xs={4}>
+              <Typography
+                color="textSecondary"
+                gutterBottom variant="subtitle2"
+                sx={{fontSize: '1.0rem'}}>
+                NgU IP
+              </Typography>
+              <Typography variant="body1" sx={{
+                fontWeight: 'bold',
+                fontSize: '1.5rem'
+                }}>
+                {data.ip_address_ngu}
+              </Typography>
+            </Grid>
+          </Grid>
+        </CardContent>
+      </Card>
+    </Grid>
+  );
+}
