d67ab4b4dbb0e78e7ae0da9de6121d590a4070a3
Kenneth <63093454+Chinorea@users.noreply.github.com>
2025-06-19 11:05:01 +0800

    Added Automated API documentation using flasgger

---
+++ backend/docs_generator/__init__.py
@@ -0,0 +1,7 @@
+"""
+gNB EMC Dashboard API Documentation Generator
+Separate module for generating Swagger/OpenAPI documentation using Flasgger
+"""
+
+__version__ = "1.0.0"
+__author__ = "gNB EMC Dashboard Team"
