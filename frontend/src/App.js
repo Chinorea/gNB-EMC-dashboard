2ec0f8d31dc3468fdde0f9862ddac436165dd89b
Kenneth <63093454+Chinorea@users.noreply.github.com>
2025-06-24 10:36:05 +0800

    Removed ARP scanner for pulling of branch

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,6 @@ import { ThemeContextProvider } from './theme/ThemeContext';
 import HomePage from './HomePage';
 import NodeDashboard from './NodeDashboard';
 import MapView from './Map';
-import ArpScanner from './ArpScanner'; // Add ARP Scanner import
 import 'leaflet/dist/leaflet.css';
 import buildStaticsLQM, { getBatteryPercentage } from './utils';
 import NodeInfo from './NodeInfo'; // Ensure NodeInfo is imported
@@ -381,10 +380,6 @@ export default function App() {
                 element={<MapView 
                   markers={mapMarkers} 
                   linkQualityMatrix={lqm} />}
-              />
-              <Route
-                path="/arp-scanner"
-                element={<ArpScanner />}
               />
             </Routes>
           </Box>
